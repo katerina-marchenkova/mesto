7f5878c87284da35a97f4c93b4df8c875070857d
Katerina Marchenkova <ekaterina.marchenkova@gmail.com>
2021-08-04 04:24:17 +0300

    fix: btn-close naming

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,7 +1,7 @@
 export function PopupComponent () {
     // todo: move selectors from ctor
     let _popup = document.querySelector('.popup');
-    let _btnClose = _popup.querySelector('.popup__btn_close');
+    let _btnClose = _popup.querySelector('.popup__btn-close');
 
     let self = this; // old style aka in jquery
 
