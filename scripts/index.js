cdce64661cee24749c4cde6238a1f515241d476e
Katerina Marchenkova <ekaterina.marchenkova@gmail.com>
2021-08-06 13:47:46 +0300

    fix: attempt to guess how to pass the code review

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,32 @@
 /* See the proper code (attempt to separate somehow data, services and view components) within the separate brach feature/js-components
 This one is my next attempt to guess how to pass the task.
 */
-/* defined constant selectors */
-const selectors = {
-  popup: ".popup",
-  popupBtnClose: ".popup__btn-close",
-  profile: ".profile",
-  profileBtnEdit: ".profile__btn-edit",
-  profileName: ".profile__title",
-  profileJob: ".profile__description",
-  profileForm: ".form",
-  profileNameInput: ".form__item_el_name",
-  profileJobInput: ".form__item_el_job"
-}
-
-/* defined constant styles */
-const cssClasses = {
-  popupOpened: "popup_opened"
-}
-
 /* internal component variables made global due to:
 Все DOM элементы нужно один раз найти при помощи метода document.querySelector и записать в переменные в начале файла, а затем использовать эти переменные в тех функциях, где они нужны.
 */
-const popupElm = document.querySelector(selectors.popup);
-const popupBtnClose = popupElm.querySelector(selectors.popupBtnClose);
-const profileElm = document.querySelector(selectors.profile);
-const profileBtnEdit = profileElm.querySelector(selectors.profileBtnEdit);
-const profileNameElm = profileElm.querySelector(selectors.profileName);
-const profileJobElm = profileElm.querySelector(selectors.profileJob);
-const profileForm = document.querySelector(selectors.profileForm);
-const profileNameInput = document.querySelector(selectors.profileNameInput);
-const profileJobInput = document.querySelector(selectors.profileJobInput);
+const popupElm = document.querySelector(".popup");
+const popupBtnClose = popupElm.querySelector(".popup__btn-close");
+const profileElm = document.querySelector(".profile");
+const profileBtnEdit = profileElm.querySelector(".profile__btn-edit");
+const profileNameElm = profileElm.querySelector(".profile__title");
+const profileJobElm = profileElm.querySelector(".profile__description");
+const profileForm = document.querySelector(".form");
+const profileNameInput = document.querySelector(".form__item_el_name");
+const profileJobInput = document.querySelector(".form__item_el_job");
 
 /* The mixture of popup and profile component logic (no incapsulation) is only per request:
 функция открытия попап (в класс должен добавиться модификатор).
 В ней же текстовые значения профайла записываются в значения инпутов */
 function openPopup() {
-  popupElm.classList.add(cssClasses.popupOpened);
+  popupElm.classList.add("popup_opened");
   // todo: register event listener for close event and escape here
   // bad code - has nothing to do with popup
-  profileNameInput.value = profileNameElm.textContent.trim(); // or use the innerText
-  profileJobInput.value = profileJobElm.textContent.trim();
+  profileNameInput.value = profileNameElm.textContent;
+  profileJobInput.value = profileJobElm.textContent;
 }
 
 function closePopup() {
-  popupElm.classList.remove(cssClasses.popupOpened);
+  popupElm.classList.remove("popup_opened");
   // todo: unsubscribe document from event listener for close event and escape here
 }
 
@@ -56,8 +38,8 @@ function updateProfile(name, job) {
 }
 
 profileBtnEdit.addEventListener("click", openPopup);
-profileForm.addEventListener("submit", (evt) => {
+profileForm.addEventListener("submit", function(evt) {
   evt.preventDefault();
-  updateProfile(profileNameInput.value.trim(), profileJobInput.value.trim()); // form value? this.value.Name
+  updateProfile(profileNameInput.value, profileJobInput.value);
   closePopup();
 });
