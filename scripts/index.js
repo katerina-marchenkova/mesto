a40aca168ccfa09a414a94c82237184bc423eb3b
Katerina Marchenkova <ekaterina.marchenkova@gmail.com>
2021-08-07 23:15:14 +0300

    feat: cards dymanic build

--- scripts/index.js
+++ scripts/index.js
@@ -13,6 +13,53 @@ const profileJobElm = profileElm.querySelector(".profile__description");
 const profileForm = document.querySelector(".form");
 const profileNameInput = document.querySelector(".form__item_el_name");
 const profileJobInput = document.querySelector(".form__item_el_job");
+const placesContainer = document.querySelector(".places__list");
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const buildPlaceCard = function(titleValue, imageLinkValue) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const imgElement = cardElement.querySelector('.card__image');
+  imgElement.setAttribute('src', imageLinkValue);
+  imgElement.setAttribute('alt', titleValue);
+  cardElement.querySelector('.card__title').textContent = titleValue;
+
+  return cardElement;
+}
+
+const addPlaceCard = function(item, index) {
+  debugger;
+  const placeElement = buildPlaceCard(item.name, item.link);
+  placesContainer.append(placeElement);
+};
+
+initialCards.map(addPlaceCard);
 
 /* The mixture of popup and profile component logic (no incapsulation) is only per request:
 функция открытия попап (в класс должен добавиться модификатор).
