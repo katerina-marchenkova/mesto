919f9c16e967b3d398be00ef17676fae51c15418
Katerina Marchenkova <ekaterina.marchenkova@gmail.com>
2021-08-10 00:29:37 +0300

    feat: new place functionality added

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,18 @@ This one is my next attempt to guess how to pass the task.
 /* internal component variables made global due to:
 Все DOM элементы нужно один раз найти при помощи метода document.querySelector и записать в переменные в начале файла, а затем использовать эти переменные в тех функциях, где они нужны.
 */
-const popupElm = document.querySelector(".popup");
-const popupBtnClose = popupElm.querySelector(".popup__btn-close");
-const profileElm = document.querySelector(".profile");
-const profileBtnEdit = profileElm.querySelector(".profile__btn-edit");
-const profileNameElm = profileElm.querySelector(".profile__title");
-const profileJobElm = profileElm.querySelector(".profile__description");
-const profileForm = document.querySelector(".form");
-const profileNameInput = document.querySelector(".form__item_el_name");
-const profileJobInput = document.querySelector(".form__item_el_job");
-const placesContainer = document.querySelector(".places__list");
+const popupElm = document.querySelector('.popup');
+const popupBtnClose = popupElm.querySelector('.popup__btn-close');
+const popupContainer = document.querySelector('.popup__container');
+const profileElm = document.querySelector('.profile');
+const profileBtnEdit = profileElm.querySelector('.profile__btn-edit');
+const profileBtnAddPlace = profileElm.querySelector('.profile__btn-add');
+const profileNameElm = profileElm.querySelector('.profile__title');
+const profileJobElm = profileElm.querySelector('.profile__description');
+const profileForm = document.querySelector('.form');
+const profileNameInput = document.querySelector('.form__item_el_name');
+const profileJobInput = document.querySelector('.form__item_el_job');
+const placesContainer = document.querySelector('.places__list');
 
 const initialCards = [
   {
@@ -66,6 +68,21 @@ initialCards.map((item) => {
   placesContainer.append(placeElement);
 });
 
+const buildNewPlacePopup = function() {
+  const newPlaceFormTemplate = document.querySelector('#new-place-form').content;
+  const newPlaceForm = newPlaceFormTemplate.querySelector('.form').cloneNode(true);
+  newPlaceForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
+    const newPlaceElm = buildPlaceCard(formData.get('Title'), formData.get('Url'));
+    placesContainer.prepend(newPlaceElm);
+    closePopup();
+    evt.target.remove();
+  });
+
+  return newPlaceForm;
+}
+
 /* The mixture of popup and profile component logic (no incapsulation) is only per request:
 функция открытия попап (в класс должен добавиться модификатор).
 В ней же текстовые значения профайла записываются в значения инпутов */
@@ -96,4 +113,10 @@ function onProfileSubmitted(evt) {
 }
 
 profileBtnEdit.addEventListener("click", openPopup);
+profileBtnAddPlace.addEventListener('click', () => {
+  const newPlaceForm = buildNewPlacePopup();
+  popupContainer.append(newPlaceForm);
+  openPopup();
+});
+
 profileForm.addEventListener("submit", onProfileSubmitted);
